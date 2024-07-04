dc1daa067be9727872fb49eb6397909f7a6cade5
Ani Maghradze <animaghradze50@gmail.com>
2024-07-05 02:22:30 +0400

    added form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Header from "./components/Header";
 import Form from "./components/Form";
+import FirstName from "./components/FirstName";
+import LastName from "./components/LastName";
+import Email from "./components/Email";
+import Button from "./components/Button";
 
 function App() {
   return (
       <div className='app'>
           <Header/>
-          <Form/>
+          <Form>
+              <FirstName/>
+              <LastName/>
+              <Email/>
+              <Button/>
+          </Form>
       </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,8 @@
-import FirstName from './FirstName'
-import LastName from './LastName'
-import Email from './Email'
-import Button from "./Button";
-const Form = () => {
+const Form = ({children}) => {
     return(
-        <div>
-            <FirstName/>
-            <LastName/>
-            <Email/>
-            <Button/>
-        </div>
+        <form>
+            {children}
+        </form>
     )
 }
 export default Form;
